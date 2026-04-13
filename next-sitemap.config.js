2a3391a979312765271efc20041a557bfd5b6195
Muhammad Hasnain <muhammad.hasnain142002@gmail.com>
2026-04-14 02:27:10 +0500

    chore: remove next-sitemap dependency and manually configure static sitemap paths

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   siteUrl: "https://thedailymixa.com",
   generateRobotsTxt: true,
+
+  additionalPaths: async () => [
+    { loc: "/", priority: 1 },
+    { loc: "/archive", priority: 0.8 },
+    { loc: "/about", priority: 0.5 },
+    { loc: "/contact", priority: 0.4 },
+    { loc: "/fashion", priority: 0.7 },
+    { loc: "/house", priority: 0.7 },
+    { loc: "/lifestyle", priority: 0.7 },
+  ],
 };
